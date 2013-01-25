49d5b499f5c48d0ba2698e996dd5b3b67f3c5406
Michael Merrild <michael.merrild@gmail.com>
2013-01-25 23:55:00 +0100

    Adding Chef metadata file

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+maintainer "Michael Merrild"
+license "Apache 2.0"
+description "Install and configure a LAMP stack"
+version "1.0.0"
+
+depends "apt"
+depends "apache2"
+depends "openssl"
+depends "mysql"
+depends "php"
